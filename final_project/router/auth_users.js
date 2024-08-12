ed84eb4e5c28d07bc9843b603a6e71d0da860b4a
PatrickCi <pc.patrick47@gmail.com>
2024-08-11 20:03:40 -0400

    Update auth_users.js
    Added additional modifying functions and delete functions.

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,26 +3,104 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [{"username":"andrew","password":"1234"}];
 
 const isValid = (username)=>{ //returns boolean
-//write code to check is the username is valid
+    //Task6 - Based on Practice project
+    // Filter the users array for any user with the same username
+    let userswithsamename = users.filter((user) => {
+        return user.username === username;
+    });
+    // Return true if any user with the same username is found, otherwise false
+    if (userswithsamename.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
+
 }
 
 const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+    //Task6 - Based on Practice project - write code to check if username and password match the one we have in records.
+
+    // Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Write your code here
+    const username = req.body.username;
+    const password = req.body.password;
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
+
 });
 
-// Add a book review
+
+// Add/Modify a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    //Task 8 - Write your code here
+    const username = req.session.authorization.username;  
+    const isbn = req.params.isbn;
+    const review = req.body.review;
+    
+    if (typeof books[isbn] != undefined) {
+        books[isbn].reviews[username] = review;
+        return res.status(200).send("Review successfully sent.");
+    }
+    else 
+        return res.status(404).json({message: "ISBN " + isbn + " not found."});
+    
+    
+});
+
+// Add/Modify a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    //Task 8 - Write your code here
+    const username = req.session.authorization.username;  
+    const isbn = req.params.isbn;
+        
+    if (typeof books[isbn] != undefined) 
+    {
+        if(typeof books[isbn].reviews[username] != undefined)
+        {
+            delete books[isbn].reviews[username];
+            return res.status(200).send("Review successfully deleted");
+        }
+        else 
+            return res.status(404).json({message: "Review not found."});
+        
+        
+    }
+    else 
+        return res.status(404).json({message: "ISBN " + isbn + " not found."});
+    
+    
 });
 
 module.exports.authenticated = regd_users;
