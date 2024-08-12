e75467f2714f557da4325c784720021f4925e6bc
PatrickCi <pc.patrick47@gmail.com>
2024-08-11 21:15:30 -0400

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -27,20 +27,34 @@ public_users.post("/register", (req,res) => {
 
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
-  //Write your code here
-   res.send(JSON.stringify(books,null,4));
+  //Task 1: Get Book List
+   //return res.send(JSON.stringify(books,null,4));
+
+   //Task 10: Get Book List as a Promise
+   getAllBooks().then(
+    (bk)=>res.send(JSON.stringify(bk, null, 4)),
+    (error) => res.send("denied")
+  );  
+
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Task 2: ISBN is the index of the array
   
   const isbn = req.params.isbn;
+  //Task 2: ISBN is the index of the array
+  /*
   if(typeof books[isbn] == 'undefined')
     res.status(404).json({message: "This ISBN could not be found."});
   else
     res.send(books[isbn]);
-    
+  */
+
+  //Task 11: Use Promise to fetch book by ISBN
+  getBookFromISBN(isbn).then(
+    (myBook)=>res.send(myBook),
+    (error) => res.send("denied")
+  );  
   
  });
   
@@ -48,12 +62,30 @@ public_users.get('/isbn/:isbn',function (req, res) {
 public_users.get('/author/:author',function (req, res) {
   //Task 3: Do a fuzzy search for near match of author else return 404
   const author = req.params.author;
-  if(author != "")
+
+
+  if(author == "")
+    return res.status(400).json({message: "There was no author provided."});
+  
+    var foundBooks = [];
+
   for(const field in books)
     if(books[field].author.toLowerCase().includes(author.toLowerCase()))
-        return res.send(books[field]);
+        foundBooks.push(books[field])
 
-  return res.status(404).json({message: "This author could not be found."});
+    if(foundBooks.length > 0)
+        return res.send(foundBooks);
+    else
+        return res.status(404).json({message: "This author could not be found."});
+
+  /*
+  //Task 12: Do a fuzzy search for near match of author using async-await
+    getBooksFromAuthor(author).then(
+    (myBooks)=>res.send(myBooks),
+    (error) => res.send("denied")
+    
+  );  
+    */
   
 });
 
@@ -61,12 +93,29 @@ public_users.get('/author/:author',function (req, res) {
 public_users.get('/title/:title',function (req, res) {
   //Task 4: Do a fuzzy search for near match of title else return 404
   const title = req.params.title;
-  if(title != "")
+  
+  if(title == "")
+    return res.status(400).json({message: "There was no title provided."});
+
+  var foundBooks = [];
+
   for(const field in books)
     if(books[field].title.toLowerCase().includes(title.toLowerCase()))
-        return res.send(books[field]);
+        foundBooks.push(books[field]);        
 
-  return res.status(404).json({message: "This book could not be found."});
+  if(foundBooks.length > 0)
+    return res.send(foundBooks);
+   else
+    return res.status(404).json({message: "This book could not be found."});
+
+  //Task 13 - Find books by title
+/*
+  getBooksFromTitle(title).then(
+    (myBooks)=>res.send(myBooks),
+    (error) => res.send("denied")
+    
+  );
+*/
 });
 
 //  Get book review
@@ -80,5 +129,57 @@ public_users.get('/review/:isbn',function (req, res) {
     
     return res.status(404).json({message: "The ISBN you are looking for could not be found."});
 });
+
+//Tasks 10-13 Async-Await / Promises
+
+function getAllBooks(){
+  //Task 10 - Setup a promise of fetching all books
+  return new Promise((resolve,reject)=>{
+    resolve(books);
+  })
+}
+
+function getBookFromISBN(isbn){
+  //Task 11 - Setup a promise of fetching a book by ISBN
+    
+  return new Promise((resolve,reject)=>{
+    if (typeof books[isbn] != undefined && isbn != "") {
+      resolve(books[isbn]);
+    }else{
+      reject("Sorry, that book was unable to be found.");
+    }    
+  })
+}
+
+function getBooksFromAuthor(author){
+  //Task 12 - Fetch all books based on Author using fuzzy search
+  let foundResults = [];
+  return new Promise((resolve,reject)=>{
+
+    for(let field in books)
+        if(books[field].author.toLowerCase().includes(author.toLowerCase()))
+        {
+            foundResults.push(books[field]);
+        }
+
+    resolve(foundResults);  
+  });
+}
+
+function getBooksFromTitle(title){
+  //Task 13 - Fetch all books based on Title using fuzzy search
+  let foundResults = [];
+  return new Promise((resolve,reject)=>{
+
+    for(let field in books)
+        if(books[field].title.toLowerCase().includes(title.toLowerCase()))
+        {
+            foundResults.push(books[field]);
+        }
+
+    resolve(foundResults);  
+  });
+}
+
 
 module.exports.general = public_users;
